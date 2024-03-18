0c5e1550db9ae76d5064b5f4d1ca13b46bcf77a4
eroktopus <147647841+eroktopus@users.noreply.github.com>
2024-03-17 20:49:55 -0400

    added bootstrap styling
    styled the login, signup, and movie card views using the grid system

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,24 +1,28 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
-// The MovieCard function component
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.Title}
-    </div>
+    <Card>
+      <Card.Img variant="top" src={movie.imageurl} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Director}</Card.Text>
+        <Button  style={{ backgroundColor: "dodgerblue", color: "white" }} 
+        onClick={() => onMovieClick(movie)} variant="link">
+          Open
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
 // Here is where we define all the props constraints for the MovieCard
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    author: PropTypes.string,
+    Title: PropTypes.string.isRequired,
+    imageurl: PropTypes.string.isRequired,
+    Director: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired,
 };
